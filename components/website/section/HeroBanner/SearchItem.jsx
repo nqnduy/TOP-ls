72431f48d943319d9356800f40b72727dbfc0d80
nqnduy <nqnduy>
2022-05-26 11:18:55 +0700

    responsive mobile

--- components/website/section/HeroBanner/SearchItem.jsx
+++ components/website/section/HeroBanner/SearchItem.jsx
@@ -1,3 +1,4 @@
+import asset from '@/plugins/assets/asset';
 import Image from "next/image";
 import React from 'react';
 
@@ -7,7 +8,7 @@ export default function SearchItem({ children }) {
             <div className="Search-filter__item">
                 <p className="txMain upc">{children}</p>
                 <div className="txMain normal purple">
-                    Select a category <Image src={asdset("/images/icons/arrow-dropdown.svg")} alt="" width={10} height={5} />
+                    Select a category <Image src={asset("/images/icons/arrow-dropdown.svg")} alt="" width={10} height={5} />
                 </div>
             </div>
             <style jsx>{`
